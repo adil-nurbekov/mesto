2d55a4d15190fc680e7ac035c9831dd80a3aafbd
Adil Nurbekov <nurbekovkz@gmail.com>
2021-04-23 23:29:45 +0600

    third atempt

---
+++ script/index.js
@@ -0,0 +1,34 @@
+const openPupupButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closePopupButton = document.querySelector(".popup__close");
+
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__profession");
+
+let inputName = document.querySelector(".popup__input_type_name");
+let inputJob = document.querySelector(".popup__input_type_job");
+
+let formEvent = document.querySelector("form");
+
+function addPopup() {
+  popup.classList.add("popup_open");
+
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup_open");
+}
+
+function addText(e) {
+  e.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  closePopup();
+}
+
+openPupupButton.addEventListener("click", addPopup);
+closePopupButton.addEventListener("click", closePopup);
+formEvent.addEventListener("submit", addText);
